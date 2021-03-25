891aabbdf356691bb29b940cc5b559cad8b08a67
David P Sanders <davepaulsanders@gmail.com>
2021-03-25 16:44:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function sayHiToGrandma(string) {
 
   var mixedCase = 'Hi there!'
 
-if (return string === string.toLowerCase){
+if (string === string.toLowerCase){
   return 'I can\'t hear you!'
 }
 }
